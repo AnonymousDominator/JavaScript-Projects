a807ad15bb20da7f57a1b475eab3d4238fb01c65
AnonymousDominator <100088395+AnonymousDominator@users.noreply.github.com>
2023-03-07 20:18:42 +0800

    Update and rename Kelvin Weather to Kelvin Weather.js

---
+++ Kelvin Weather.js
@@ -0,0 +1,11 @@
+//kelvin 
+const kelvin = 293;
+//celsius
+const celsius = kelvin - 273;
+
+var fahrenheit = celsius*(9/5) + 32
+
+//fahrenheit
+console.log(Math.floor(fahrenheit))
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
